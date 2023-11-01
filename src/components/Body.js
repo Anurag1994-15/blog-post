cf8652ddba98d10b3cf976b3604725dd53e09500
Anurag1994-15 <anurag.singh@yash.com>
2023-11-01 14:14:18 +0530

    second commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Login'
 import Browse from './Browse'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, useNavigate } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom'
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/firebase'
+import { useDispatch } from 'react-redux'
+import { addUser, removeUser } from '../utils/userSlice'
 
 const Body = () => {
+const dispatch=useDispatch()
 
     const appRouter = createBrowserRouter([
         {
@@ -16,6 +21,19 @@ const Body = () => {
             element:<Browse/>
         }
     ])
+
+    useEffect(()=>{
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const {uid,eamil,displayName} = user;
+          dispatch(addUser({uid:uid,eamil:eamil,displayName:displayName}));
+         
+        } else {
+          dispatch(removeUser());
+        
+        }
+      });
+    },[])
 
   return (
     <div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="absolute px-8 py-2 z-10" >
+    <div className="absolute px-4 py-2 z-10" >
       <img
-       className='w-44'
+       className="w-32 "
        src="https://img.freepik.com/premium-vector/word-concept-color-geometric-shapes-blog_205544-13021.jpg"
        alt="logo"
        />
