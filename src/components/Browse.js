e6841afdfdf1f9c19125d9be325a200c58861072
Anurag1994-15 <anurag.singh@yash.com>
2023-11-06 16:09:05 +0530

    post first commit

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
-
+import "../style.css";
+import DisplayAllPosts from './DisplayAllPosts'
 const Browse = () => {
   return (
     <div>
       <Header/>
+      <DisplayAllPosts />
     </div>
   )
 }
